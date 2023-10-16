b85daa712c793b072bb89b1edda4a10eaea741df
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-16 14:12:02 +0200

    Add project files.

---
+++ 5.B Temperature/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.B_Temperature
+{
+    internal class Program
+    {
+        public static bool test (int temp1, int temp2)
+        { 
+            return temp1 < 0 && temp2 > 100 || temp2 < 0 && temp1> 100;        
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(120, -1));
+            Console.WriteLine(test(-1, 120));
+            Console.WriteLine(test(2, 120));
+            Console.ReadKey();
+
+        }
+    }
+}
